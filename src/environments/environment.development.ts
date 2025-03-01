44fc56c636dc1d314f0b3a4c26430180efff96f4
Amaury Franssen <amauryfranssen@gmail.com>
2025-03-01 12:12:19 +0100

    initial

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  baseUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000/api',
+  // You will need to create a Firebase project and replace the configuration here with yours
+  firebase: {
+    apiKey: "AIzaSyDwtB8c1BsnVI6R8dwHc9S5yl6DY6IEFWA",
+    authDomain: "quizzam.firebaseapp.com",
+    projectId: "quizzam",
+    storageBucket: "quizzam.firebasestorage.app",
+    messagingSenderId: "316827895691",
+    appId: "1:316827895691:web:9972a41a493d1222b1d18e"
+  },
+  useSocketIo: true
+};
